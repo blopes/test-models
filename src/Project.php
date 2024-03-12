74218c3e7d30290e42c9af37178e3942ed92908f
blopes <blopes@bimms.net>
2024-03-12 13:45:27 +0000

    Update Project.php

--- src/Project.php
+++ src/Project.php
@@ -2,12 +2,7 @@
 
 namespace MSource\SharedModels;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
