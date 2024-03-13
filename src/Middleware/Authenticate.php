b2c9d67706fc324ff192a75110c3a422e522ad95
blopes <blopes@bimms.net>
2024-03-13 15:18:58 +0000

    Update Authenticate.php

--- src/Middleware/Authenticate.php
+++ src/Middleware/Authenticate.php
@@ -56,6 +56,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : "route('login')";
     }
 }
