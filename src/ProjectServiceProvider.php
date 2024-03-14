adfac0aea21ab8c8aebdbe1f8df9c59949771f35
blopes <blopes@bimms.net>
2024-03-14 12:37:29 +0000

    Update ProjectServiceProvider.php

--- src/ProjectServiceProvider.php
+++ src/ProjectServiceProvider.php
@@ -22,6 +22,19 @@ class ProjectServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $seed_list[] = 'Blopes\SharedModels\Database\Seeders';
+
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadSeeders($seed_list);
     }
+
+    protected function loadSeeders($seed_list){
+        $this->callAfterResolving(DatabaseSeeder::class, function ($seeder) use ($seed_list) {
+                    foreach ((array) $seed_list as $path) {
+                        $seeder->call($seed_list);
+                        // here goes the code that will print out in console that the migration was succesful
+                    }
+                });
+            }
+        
 }
