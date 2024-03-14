acc30e6e8b700fb118bea82a90865492e36dc071
blopes <blopes@bimms.net>
2024-03-14 12:22:46 +0000

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,10 @@ namespace Blopes\SharedModels\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Blopes\SharedModels\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use function Symfony\Component\Clock\now;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +18,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(10)->create();
+        DB::table('users')->insert([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john.doe@example.com',
+            'password' => Hash::make('password123'), // Replace 'password123' with the desired password
+            'thumbnail_id' => 1, // Replace with the desired thumbnail_id
+            'picture' => 'path_to_picture.jpg', // Replace 'path_to_picture.jpg' with the actual path to the picture
+            'phone_number' => '1234567890', // Replace '1234567890' with the desired phone number
+            'email_verified_at' => now(),
+            'remember_token' => null, // It will be automatically generated when needed
+            'created_at' => now(),
+            'updated_at' => now(),
+            'deleted_at' => null,
+        ]);
     }
 }
