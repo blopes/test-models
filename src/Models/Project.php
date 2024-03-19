7433a3470ea36615e8916908a09cd22455031299
blopes <blopes@bimms.net>
2024-03-19 16:52:23 +0000

    Update Project.php

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -11,6 +11,7 @@ use Blopes\SharedModels\Models\StageChapters;
 use Blopes\SharedModels\Models\Thumbnail;
 use Blopes\SharedModels\Models\Unit;
 use Blopes\SharedModels\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
