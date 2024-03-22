d696a14374167eb2f42ca1781861427f138d66b1
blopes <blopes@bimms.net>
2024-03-22 10:26:14 +0000

    add user factory

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use Blopes\SharedModels\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => $this->now(),
+            'password' => 'password',
+            'thumbnail_id' => 1
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(
+            fn (array $attributes) => [
+                'email_verified_at' => null,
+            ]
+        );
+    }
+}
