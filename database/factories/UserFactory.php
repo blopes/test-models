a323503196850ac585252266129411f1c0821cc8
blopes <blopes@bimms.net>
2024-03-22 10:28:56 +0000

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Blopes\SharedModels\Database\Factories;
 
 use Blopes\SharedModels\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
